8e1dd5bf7708ff494ac9e9df94f818620d01fecf
Tohfa Akib <tohfa@filerskeepers.co>
2026-02-10 19:59:13 +0600

    latest code draft pushed

--- jimmy_crawler/spiders/usa/california_wildlife.py
+++ jimmy_crawler/spiders/usa/california_wildlife.py
@@ -1,50 +1,55 @@
+import scrapy
+import asyncio
 from jimmy_crawler.spiders.base import BaseJimmySpider
 from jimmy_crawler.utils import format_date, convert_to_markdown, clean_text
 
 
 class CaliforniaWildlifeSpider(BaseJimmySpider):
     name = "usa_california_wildlife"
-    start_urls = ["https://wildlife.ca.gov/News/Archive"]
 
-    def parse(self, response):
+    # --- MODE A: DISCOVERY (Scanning) ---
+    def build_discovery_requests(self, seed: dict):
+        # Start at the archive page
+        yield scrapy.Request("https://wildlife.ca.gov/News/Archive", callback=self.parse_discovery)
+
+    def parse_discovery(self, response):
+        found_links = []
         for article in response.css('.post_article'):
             link = article.css('.la_title h3 a::attr(href)').get()
             if link:
-                yield response.follow(link, callback=self.parse_detail)
+                found_links.append({
+                    'url': response.urljoin(link),
+                    'metadata': {'source': response.url}
+                })
 
+        # Send to Brain
+        if found_links:
+            asyncio.run(self.store_discovered_links(found_links))
+
+        # Pagination
         next_page = response.css('.pager a.PageNext::attr(href)').get()
         if next_page:
-            yield response.follow(next_page, callback=self.parse)
+            yield response.follow(next_page, callback=self.parse_discovery)
 
+    # --- MODE B: FETCHING (Scraping) ---
+    # This is called automatically by BaseJimmySpider for URL_BATCH tasks
     def parse_detail(self, response):
         title = response.css('.la_title h3 a::text').get()
+        date_obj = format_date(response.css('.article_date::text').get(), date_order="MDY")
 
-        # The site uses "Month Day, Year" so date_order="MDY" is safer,
-        # but "DMY" usually works for written months too.
-        raw_date_str = response.css('.article_date::text').get()
-        date_obj = format_date(raw_date_str, date_order="MDY")
+        clean_content = clean_text(response.css('.article_summary ::text').getall())
+        markdown = convert_to_markdown(response.css('.article_summary').get(), include_images=True)
 
-        # 3. Content - Raw Clean Text
-        raw_text_list = response.css('.article_summary ::text').getall()
-        clean_content = clean_text(raw_text_list)
-
-        # 4. Content - Markdown
-        raw_html = response.css('.article_summary').get()
-        markdown_content = convert_to_markdown(raw_html, include_images=True, include_tables=False) # Images included, tables excluded; not tested yet, just for sample
-
-        # 5. Keywords and Tags
         categories = response.css('.article_categories a::text').getall()
         tags = response.css('.article_tags a::text').getall()
-
-        combined_keywords = list(set(categories + tags))
 
         yield self.build_item(
             response=response,
             title=title,
             date_of_publication=date_obj,
             content=clean_content,
-            content_markdown=markdown_content,
+            content_markdown=markdown,
             jurisdiction="California",
-            legal_classification="legislation",
-            source_keywords=combined_keywords,
+            legal_classification="press_release",
+            source_keywords=list(set(categories + tags)),
         )
